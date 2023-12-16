611aa7f8383ec1e05e3175173af0ca7e3036e47f
Chuan1337 <69002981+Chuan212@users.noreply.github.com>
2023-12-16 11:54:43 +0800

    Add files via upload
    update

---
+++ Offsets.hpp
@@ -0,0 +1,28 @@
+//build by Chuan212  for China version
+auto GameBase = 0x140000000;
+namespace Offsets{ 
+    constexpr auto WorldPtr            = 0x7821870;
+    constexpr auto GObject             = 0x7821840;
+    constexpr auto NamePool            = 0x7a3d740;
+    constexpr auto NameKeyPtr          = 0x7821820;
+    constexpr auto ULevel              = 0x38;
+    constexpr auto GameState           = 0x140;
+    constexpr auto PlayerArray         = 0x3f0;
+    constexpr auto GameInstance        = 0x1a0;
+    constexpr auto LocalPlayers        = 0x40;
+    constexpr auto PlayerController    = 0x38;
+    constexpr auto AcknowledgedPawn    = 0x468;
+    constexpr auto PlayerCameraManager = 0x480;
+    constexpr auto CameraCachePrivate  = 0x2650;
+    constexpr auto PlayerState         = 0x3f8;
+    constexpr auto Mesh                = 0x438;
+    constexpr auto BoneArray           = 0x5c8;
+    constexpr auto PlayerCameraManager = 0x480;
+    constexpr auto CameraCachePrivate  = 0x2650;
+    constexpr auto TeamComponent       = 0x630;
+    constexpr auto BombTeam            = 0xf8;
+    constexpr auto DamageHandler       = 0x9f0;
+    constexpr auto CachedLife          = 0x1b0;
+    constexpr auto Inventory           = 0x990;
+    constexpr auto CurrentEquippable   = 0x248;
+} 
